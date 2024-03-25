6b7c738795c9daad2ccd8a15d10a42e304482791
iGEORGE <www.igeorge.www@gmail.com>
2024-03-26 00:31:01 +0100

    fixed post update 1

--- src/app/api/posts/[id]/route.ts
+++ src/app/api/posts/[id]/route.ts
@@ -35,21 +35,48 @@ export async function GET(request: NextRequest, { params }: any) {
 
 }
 
-export async function PUT() {
+export async function PUT(request: NextRequest, { params }: any) {
     try {
 
-        const updatePosts = await prisma.post.updateMany({
+        const { title, tag, content, published, categoryId, imageUrl, publicId, authorId } = await request.json()
+
+        const post = await prisma.post.findUnique({
+            where: {
+                id: params.id
+            }
+        })
+
+        if(!post) {
+            return NextResponse.json({ message: "An error occurred...." }, { status: 400 })
+        }        
+
+        const updatedPost = await prisma.post.update({
+            where: {
+                id: params.id
+            },
             data: {
-                views: {
-                increment: 1,
+                title: title || post?.title,
+                tag: tag || post?.tag,
+                content: content || post?.content,
+                published: published || post?.published,
+                category: {
+                    connect: {
+                        id: categoryId || post?.categoryId
+                    }
                 },
-                likes: {
-                increment: 1,
-                },
+                imageUrl: imageUrl || post?.imageUrl, 
+                publicId: publicId || post?.publicId,
+                author: {
+                    connect: {
+                        id: authorId
+                    }
+                }
             },
             })
+
+            return NextResponse.json({ updatedPost }, { status: 500 })
         
-    } catch (error) {
-        
+    } catch (error: any) {
+        return NextResponse.json({ error: error.message }, { status: 500 })
     }
 }
